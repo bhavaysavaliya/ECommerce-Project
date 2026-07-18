3fd7ac46103278894d3a6933519364f55a146ddc
Bhavay Savaliya <bvysavaliya@gmail.com>
2026-07-18 17:39:41 +0530

    Added distributed logging using opentelemetry, loki and grafana

--- ecommerce-gateway/src/main/java/com/project/ecommerce/gateway/EcommerceGatewayApplication.java
+++ ecommerce-gateway/src/main/java/com/project/ecommerce/gateway/EcommerceGatewayApplication.java
@@ -2,11 +2,14 @@ package com.project.ecommerce.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class EcommerceGatewayApplication {
 
 	public static void main(String[] args) {
+		// helps in logging of traceid and spanid which are created at gateway
+		Hooks.enableAutomaticContextPropagation();
 		SpringApplication.run(EcommerceGatewayApplication.class, args);
 	}
 
